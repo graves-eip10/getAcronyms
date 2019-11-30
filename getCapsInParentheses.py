601a7ab288099755ccbb35b971d2701049e79598
graves-eip10 <57725037+graves-eip10@users.noreply.github.com>
2019-11-30 08:37:28 -0800

    Add files via upload

---
+++ getCapsInParentheses.py
@@ -0,0 +1,32 @@
+import re
+from docx import Document
+from openpyxl import Workbook
+
+print("Please enter the document name:")
+docName = input()
+
+doc = Document(docName)
+
+#Prepare an accumulator for the file contents
+fullText = ""
+
+for paragraph in doc.paragraphs:
+	fullText += paragraph.text
+
+#Parse the full text of the document and return only the caps in parentheses
+capsInParentheses = re.findall('\(([A-Z]+)\)', fullText)
+
+acronymSink = Workbook()
+ws = acronymSink.active
+ws.title = "Acronyms"
+
+ws.cell(row = 1, column = 1).value = 'Acronyms'
+
+#Prepare a counter for the excel rows
+rowIndex = 2
+
+for i in range(0,len(capsInParentheses)):
+	ws.cell(row = rowIndex, column = 1).value = capsInParentheses[i]
+	rowIndex += 1
+
+acronymSink.save('myAcronyms.xlsx')
